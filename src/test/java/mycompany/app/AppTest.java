df2b9d2a23f48f1a73609cc48f7c0d1fc62d8b5b
Aung Thura Zaw <33411285+Thura98@users.noreply.github.com>
2024-06-21 16:19:19 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://wadawdawdawdaw";
+	String url = "http://192.168.18.16/index.php";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
